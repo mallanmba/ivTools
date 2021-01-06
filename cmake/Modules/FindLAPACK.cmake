8b6b1cf60d0817cbcd2bb3934f5466e41b194801
Mark B. Allan <mallanmba@gmail.com>
2021-01-05 18:03:35 -0800

    set build up to be an ament package so it can be included in ros2 workspaces

--- cmake/Modules/FindLAPACK.cmake
+++
@@ -1,9 +0,0 @@
-# FindLAPACK delegates to either 
-#   FindLAPACK-win32.cmake on Windows or
-#   FindLAPACK-eigen.cmake elsewhere
-
-if( WIN32 )
-  find_package( LAPACK-win32 )
-else( WIN32 )
-  find_package( LAPACK-eigen )
-endif( WIN32 )
